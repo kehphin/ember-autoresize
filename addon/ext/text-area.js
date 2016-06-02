6d476e908c101338b96f587bfc4c43cdd6f29c63
kehphin <kyang@salsify.com>
2016-06-02 00:36:49 -0400

    Add a hook to new textarea line in text-area.js

--- addon/ext/text-area.js
+++ addon/ext/text-area.js
@@ -46,6 +46,81 @@ Ember.TextArea.reopen(AutoResize, /** @scope Ember.TextArea.prototype */{
       }
       return value + '@';
     }
-  })
+  }),
+  
+  /**
+    Alter the `dimensions` property of the
+    view to conform to the measured size of
+    the view.
+
+    @method measuredSizeDidChange
+   */
+  measuredSizeDidChange: on('didInsertElement', observer('measuredSize', function () {
+    let size = get(this, 'measuredSize');
+    if (size == null) { return; }
+
+    let { maxWidth, maxHeight } = size;
+    let layoutDidChange = false;
+    let dimensions = {};
+
+    if (get(this, 'shouldResizeWidth')) {
+      // Account for off-by-one error in FireFox
+      // (specifically, input elements have 1px
+      //  of scroll when this isn't applied)
+      // TODO: sniff for this bug and fix it!
+      size.width += 1;
+
+      if (maxWidth != null &&
+          size.width > maxWidth) {
+        dimensions.width = maxWidth;
+      } else {
+        dimensions.width = size.width;
+      }
+      layoutDidChange = true;
+    }
+
+    if (get(this, 'shouldResizeHeight')) {
+      if (size.height !== dimensions.height) {
+        if (maxHeight != null &&
+            size.height > maxHeight) {
+          dimensions.height = maxHeight;
+        } else {
+          dimensions.height = size.height;
+        }
+        layoutDidChange = true;
+      }
+    }
+
+    set(this, 'dimensions', dimensions);
+
+    if (layoutDidChange) {
+      scheduleOnce('render', this, 'dimensionsDidChange');
+    }
+  })),
+
+  /**
+    Retiles the view at the end of the render queue.
+    @method dimensionsDidChange
+   */
+  dimensionsDidChange() {
+    var dimensions = get(this, 'dimensions');
+    var styles = {};
+
+    for (let key in dimensions) {
+      if (!dimensions.hasOwnProperty(key)) { continue; }
+      styles[key] = dimensions[key] + 'px';
+    }
+
+    if (get(this, 'maxHeight') == null) {
+      styles.overflow = 'hidden';
+    }
+
+    var $element = this.$();
+    if ($element) {
+      $element.css(styles);
+    }
+    
+    this.sendAction('on-resize');
+  }
 
 });
